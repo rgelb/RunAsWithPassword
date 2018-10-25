02d1498755fc8b7c586c6c43c4f30b156c2e7e43
Robert Gelb <robert.gelb@xome.com>
2018-10-25 09:57:17 -0700

    Initial Checkin

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.CommandLineHelper;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunAsX {
+    class Program {
+
+        private static readonly Arguments appArgs = new Arguments();
+
+        static void Main(string[] args) {
+            if (!Parser.ParseArgumentsWithUsage(args, appArgs))
+                Environment.Exit(-1);
+
+            try {
+                AskForPassword();
+                RunAsImpersonator.Run(appArgs.File, appArgs.Domain, appArgs.Login, appArgs.Password);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void AskForPassword() {
+            // Ask for password if there is a passed in Domain Login, but no password
+            if (string.IsNullOrEmpty(appArgs.DomainLogin) || !string.IsNullOrEmpty(appArgs.Password)) return;
+
+            string pass = "";
+            Console.Write("Enter your password: ");
+            ConsoleKeyInfo key;
+
+            do {
+                key = Console.ReadKey(true);
+
+                // Backspace Should Not Work
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter) {
+                    pass += key.KeyChar;
+                    Console.Write("*");
+                }
+                else {
+                    if (key.Key == ConsoleKey.Backspace && pass.Length > 0) {
+                        pass = pass.Substring(0, (pass.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                }
+            }
+            // Stops Receving Keys Once Enter is Pressed
+            while (key.Key != ConsoleKey.Enter);
+
+            appArgs.Password = pass;
+            Console.WriteLine();
+        }
+    }
+
+
+    [CommandLineArguments(CaseSensitive = false)]
+    public class Arguments {
+        [Argument(ArgumentType.AtMostOnce, HelpText = @"Domain Login, e.g CompanyDomain\username", DefaultValue = "", LongName = "DomainLogin", ShortName = "dl")]
+        public string DomainLogin;
+        [Argument(ArgumentType.AtMostOnce, HelpText = "Password", DefaultValue = "", LongName = "Password", ShortName = "p")]
+        public string Password;
+        [Argument(ArgumentType.Required, HelpText = "File to execute", LongName = "File to execute", ShortName = "f")]
+        public string File;
+        [Argument(ArgumentType.AtMostOnce, HelpText = "Arguments", DefaultValue = "", LongName = "Arguments", ShortName = "a")]
+        public string Args;
+
+
+        // helper properties
+        public string Domain {
+            get {
+                if (string.IsNullOrEmpty(DomainLogin)) return "";
+
+                var split = DomainLogin.Split(@"\".ToCharArray());
+                if (split.Length == 2)
+                    return split[0];
+
+                return "";
+            }
+        }
+
+        public string Login {
+            get {
+                if (string.IsNullOrEmpty(DomainLogin)) return "";
+
+                var split = DomainLogin.Split(@"\".ToCharArray());
+                if (split.Length == 2)
+                    return split[1];
+
+                return "";
+            }
+        }
+    }
+}
---
+++ RunAsImpersonator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunAsX {
+    public class RunAsImpersonator {
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        struct StartupInfo {
+            public int cb;
+            public string reserved1;
+            public string desktop;
+            public string title;
+            public uint dwX;
+            public uint dwY;
+            public uint dwXSize;
+            public uint dwYSize;
+            public uint dwXCountChars;
+            public uint dwYCountChars;
+            public uint dwFillAttribute;
+            public uint dwFlags;
+            public ushort wShowWindow;
+            public short reserved2;
+            public int reserved3;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct ProcessInfo {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public uint dwProcessId;
+            public uint dwThreadId;
+        }
+
+        [Flags]
+        enum LogonFlags {
+            LOGON_WITH_PROFILE = 0x00000001,
+            LOGON_NETCREDENTIALS_ONLY = 0x00000002
+        }
+
+        [Flags]
+        enum CreationFlags {
+            CREATE_SUSPENDED = 0x00000004,
+            CREATE_NEW_CONSOLE = 0x00000010,
+            CREATE_NEW_PROCESS_GROUP = 0x00000200,
+            CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+            CREATE_SEPARATE_WOW_VDM = 0x00000800,
+            CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+        }
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
+        static extern bool CreateProcessWithLogonW(
+            string principal,
+            string authority,
+            string password,
+            LogonFlags logonFlags,
+            string appName,
+            string cmdLine,
+            CreationFlags creationFlags,
+            IntPtr environmentBlock,
+            string currentDirectory,
+            ref StartupInfo startupInfo,
+            out ProcessInfo processInfo);
+
+        public static void Run(string file, string domain, string login, string password) {
+            //var commandLine = string.Format("\"{0}\" {1}", pathToIExploreExe, arguments);
+            var commandLine = string.Format("\"{0}\"", file);
+
+            uint LOGON_NETCREDENTIALS_ONLY = 2;
+            var lpStartupInfo = new StartupInfo();
+            ProcessInfo processInformation;
+
+            CreateProcessWithLogonW(
+                        login,
+                        domain,
+                        password,
+                        (LogonFlags) LOGON_NETCREDENTIALS_ONLY,
+                        null,
+                        commandLine,
+                        0,
+                        IntPtr.Zero,
+                        null,
+                        ref lpStartupInfo,
+                        out processInformation);
+        }
+
+    }
+}
